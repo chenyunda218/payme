55016bd8a0e2b8eea0d7d8625d6c6fa11458d76c
Dan Chen <dan.chen@boardware.com>
2023-08-15 15:19:29 +0800

    fixup

--- lib.php
+++ lib.php
@@ -23,52 +23,28 @@ function Base64JS($from)
 
 function guidv4($data = null)
 {
-  // Generate 16 bytes (128 bits) of random data or use the data passed into the function.
   $data = $data ?? random_bytes(16);
   assert(strlen($data) == 16);
-
-  // Set version to 0100
   $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
-  // Set bits 6-7 to 10
   $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
-
-  // Output the 36 character UUID.
   return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
 }
 
-function createPaymentRequest($baseUrl, $token, $keyId, $secret, $merchantId)
+function createPaymentRequest($jsURL, $baseUrl, $token, $keyId, $secret, $body)
 {
   $client = new Client([
     'timeout'  => 5.0,
   ]);
-  $body = array(
-    'totalAmount' => 200,
-    'currencyCode' => 'HKD',
-    'notificationUri' => "https://example.com/return",
-  );
-  $bodyString = json_encode($body);
-  $sha256digest = hash('sha256', 'Message');
-  // echo "$sha256digest<br>";
-  $base64sha2562 = base64_encode($sha256digest);
-  // echo $base64sha2562;
-  $uuid = guidv4();
-  // echo $date->format('Y-m-d\\TH:i:sP');
-  $requestDateTime = date("Y-m-d\\TH:i:s") . '.000Z';
-  // keyId="ca7efc3f-d68a-4205-96bc-31e05ca5be2a",algorithm="hmac-sha256",headers="(request-target) Api-Version Request-Date-Time Trace-Id Authorization Digest",signature="5V6H7ZfWaU4xCxZnOsWNVBX3HVC3jsPZB9hCRSGIPiQ="
-  // keyId="ca7efc3f-d68a-4205-96bc-31e05ca5be2a",algorithm="hmac-sha256",headers="(request-target) Api-Version Request-Date-Time Trace-Id Authorization Digest",signature="ib1VYBebjemgYIbsh+DOnnRM/5wz3EoEqn++9HVfT4w="
-  $headers = array(
-    'Request-Date-Time' => $requestDateTime,
-    'Api-Version' => '0.12',
-    'Trace-Id' => $uuid,
-    'Authorization' => "Bearer $token",
-    'Digest' => '',
-  );
-  // var_dump($headers);
-
-  // $response = $client->request('POST', "$baseUrl/payments/paymentrequests", [
-  //   'headers' => $headers,
-  // ]);
-  // return json_decode($response->getBody()->__toString(), true);
+  $response = $client->request('POST', $jsURL, [
+    'json' => [
+      'api_base_url' => $baseUrl,
+      'access_token' => $token,
+      'signing_key_id' => $keyId,
+      'signing_key' => $secret,
+      'body' => $body,
+    ],
+  ]);
+  return json_decode($response->getBody()->__toString(), true);
 }
 
 function GetAccessToken($baseUrl, $clientId, $clientSecret)
--- test.php
+++ test.php
@@ -3,13 +3,46 @@ require 'lib.php';
 $a = GetAccessToken('https://sandbox.api.payme.hsbc.com.hk', '77951c89-ebcc-43de-9a85-3a61f64e4d2d', 'lH38Q~ExDWe2IGuY_-M6p_tf8LF8QcttQRcNIdma');
 $token =  $a['accessToken'];
 
-createPaymentRequest(
+$j = createPaymentRequest(
+  'https://payme-proxy.sum-foods.com/',
   "https://sandbox.api.payme.hsbc.com.hk",
   $token,
   "ca7efc3f-d68a-4205-96bc-31e05ca5be2a",
   "b0dGSEtPWjZ2QlFaQmxIS0JHeWVVVzh3T0x2SVRFamhzQXdXenZzSWdIOD0=",
-  "21092956-3359-46f6-bd5e-4378eadf8053"
+  [
+    "totalAmount" => 3.77,
+    "currencyCode" => "HKD",
+    "effectiveDuration" => 600,
+    "notificationUri" => "https=>//webhook.site/a8355331-d748-4951-bf6b-7e02f6fce605",
+    "appSuccessCallback" => "www.example.com/success",
+    "appFailCallback" => "www.example.com/failure",
+    "merchantData" => [
+      "orderId" => "ID12345678",
+      "orderDescription" => "Description displayed to customer",
+      "additionalData" => "Arbitrary additional data - logged but not displayed",
+      "shoppingCart" => [
+        [
+          "category1" => "General categorization",
+          "category2" => "More specific categorization",
+          "category3" => "Highly specific categorization",
+          "quantity" => 1,
+          "price" => 1,
+          "name" => "Item 1",
+          "sku" => "SKU987654321",
+          "currencyCode" => "HKD"
+        ],
+        [
+          "category1" => "General categorization",
+          "category2" => "More specific categorization",
+          "category3" => "Highly specific categorization",
+          "quantity" => 2,
+          "price" => 1,
+          "name" => "Item 2",
+          "sku" => "SKU678951234",
+          "currencyCode" => "HKD"
+        ]
+      ]
+    ]
+  ]
 );
-// $str = 'dan.chen';
-// echo  hash('sha256', 'Message');
-// echo date("Y-m-d\TH:i:sP");
+var_dump($j);
